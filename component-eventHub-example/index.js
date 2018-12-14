5b7252f44906073b3e805e1fbf80c7caa639fe6e
bowen-wu <13793248162@163.com>
2018-12-14 17:13:05 +0800

    feat(component-eventHub-example): use eventHub

---
+++ component-eventHub-example/index.js
@@ -0,0 +1,145 @@
+let money = {
+    amount: 100000,
+  }
+  let user = {
+    id: 123,
+    nickname: 'hello world'
+  }
+  
+  
+  
+  let callbackLists = {};
+  let eventHub = {
+    trigger(eventName, data) {
+      callbackLists[eventName] && callbackLists[eventName].forEach(callback => callback(data));
+    },
+    on(eventName, callback) {
+      if(!callbackLists[eventName]) {
+        callbackLists[eventName] = [];
+      }
+      callbackLists[eventName].push(callback)
+    },
+  }
+  
+  let store= {
+    init() {
+      eventHub.on('consume', (data) => {
+        money.amount -= data;
+        render();
+      })
+    }
+  }
+  store.init()
+  
+  
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        money,
+        user,
+      }
+    }
+    render() {
+      return (
+        <div className="root">
+          <BigPapa money={this.state.money}/>
+          <YoungPapa money={this.state.money}/>
+        </div>
+      )
+    }
+  }
+  
+  class BigPapa extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    render() {
+      return (
+        <div className="papa">
+          <p>BigPapa -> {this.props.money.amount}</p>
+          <Son1 money={this.props.money}/>
+          <Son2 money={this.props.money}/>
+        </div>
+      )
+    }
+  }
+  class YoungPapa extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    render() {
+      return (
+        <div className="papa">
+          <p>YoungPapa -> {this.props.money.amount}</p>
+          <Son3 money={this.props.money}/>
+          <Son4 money={this.props.money}/>
+        </div>
+      )
+    }
+  }
+  class Son1 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    render() {
+      return (
+        <div className="son">son1 -> {this.props.money.amount}</div>
+      )
+    }
+  }
+  class Son2 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    paid() {
+      eventHub.trigger('consume', 100);
+    }
+    render() {
+      return (
+        <div className="son">
+          <div>son2 -> {this.props.money.amount}</div>
+          <button onClick={() => this.paid()}>pay</button>
+        </div>
+      )
+    }
+  }
+  class Son3 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    render() {
+      return (
+        <div className="son">son3 -> {this.props.money.amount}</div>
+      )
+    }
+  }
+  class Son4 extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+    render() {
+      return (
+       <div className="son">son4 -> {this.props.money.amount}</div>
+      )
+    }
+  }
+  
+   
+  function render() {
+    ReactDOM.render(<App />, document.querySelector('#root'));
+  }
+  
+  render();
